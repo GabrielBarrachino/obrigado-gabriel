34d05fdda8a1cf1711c5ddedca8f6513e06e8378
Gabriel Barrachino Ribeiro <54113548+GabrielBarrachino@users.noreply.github.com>
2022-09-19 16:17:47 -0300

    Add files via upload

---
+++ obrigado.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.text', {delay: 200})
+sr.reveal('.paragrafo', {interval: 200})
+sr.reveal('.btn', {interval: 200})
